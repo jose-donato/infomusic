81a452dab8d323bc8bab18bf7c2585791c18eef0
ubuntu <zmcdonato@gmail.com>
2018-10-11 18:40:00 +0100

    rmi server and client already connecting

--- src/com/company/RMIClient.java
+++ src/com/company/RMIClient.java
@@ -1,4 +1,12 @@
 package com.company;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class RMIClient {
+    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+        Interface i = (Interface) Naming.lookup("infoMusicRegistry");
+        System.out.println(i.add(1,2));
+    }
 }
--- src/com/company/RMIServer.java
+++ src/com/company/RMIServer.java
@@ -4,30 +4,27 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class RMIServer extends UnicastRemoteObject implements Interface {
+    private static final long serialVersionUID = 1L;
+
+    protected RMIServer() throws RemoteException {
+        super();
+    }
 
 
-        private static final long serialVersionUID = 1L;
-
-        protected RMIServer() throws RemoteException {
-            super();
-        }
+    public double add(double a, double b) {
+        return a + b;
+    }
 
 
-        public double add(double a, double b) {
-            return a + b;
-        }
-
-
-        /**
-         * @param args
-         * @throws RemoteException
-         */
-        public static void main(String[] args) throws RemoteException {
-            CalculatorInterface ci = new Calculator();
-            LocateRegistry.createRegistry(1099).rebind("calc", ci);
-            System.out.println("Calculator ready...");
-        }
-
+    /**
+     * @param args
+     * @throws RemoteException
+     */
+    public static void main(String[] args) throws RemoteException {
+        Interface i = new RMIServer();
+        LocateRegistry.createRegistry(1099).rebind("infoMusicRegistry", i);
+        System.out.println("Server ready...");
     }
 
 }
+
