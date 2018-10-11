390fe26c46f47820837475bd4c463d53df7d9514
ubuntu <zmcdonato@gmail.com>
2018-10-12 00:46:58 +0100

    rmi server and client, trying to send login info over udp to multicast server

--- src/com/company/RMIClient.java
+++ src/com/company/RMIClient.java
@@ -13,16 +13,43 @@ public class RMIClient {
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         Interface i = (Interface) Naming.lookup("infoMusicRegistry");
         System.out.println("menu: (type one of the options)");
-        System.out.println("1. register/login");
-        System.out.println("2. exit");
+        System.out.println("1. login");
+        System.out.println("2. register");
+        System.out.println("3. exit");
 
         Scanner keyboard = new Scanner(System.in);
         int choice = keyboard.nextInt();
         switch(choice) {
             case 1:
-                System.out.println("username");
+                System.out.println("type your username:");
+                keyboard = new Scanner(System.in);
+                String username = keyboard.nextLine();
+                System.out.println("type your password:");
+                keyboard = new Scanner(System.in);
+                String password = keyboard.nextLine();
+
+                //if login succeeds
+                if(i.login(username, password) == 1) {
+                    //tbc
+                    System.out.println("login successful");
+                }
+                else {
+                    //tbc
+                    System.out.println("wrong credentials");
+                }
                 break;
             case 2:
+                System.out.println("type your username:");
+                keyboard = new Scanner(System.in);
+                username = keyboard.nextLine();
+                //check if username exists in database
+                System.out.println("type your password:");
+                keyboard = new Scanner(System.in);
+                password = keyboard.nextLine();
+                //check if password is certain by asking two times
+                i.register(username, password);
+                break;
+            case 3:
                 System.out.println("exiting...");
                 System.exit(0);
                 break;
--- src/com/company/RMIServer.java
+++ src/com/company/RMIServer.java
@@ -2,6 +2,7 @@ package com.company;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 public class RMIServer extends UnicastRemoteObject implements Interface {
     private static final long serialVersionUID = 1L;
@@ -12,12 +13,21 @@ public class RMIServer extends UnicastRemoteObject implements Interface {
 
 
     public int login(String username, String password) {
+        byte[] buffer = auxLogin(username, password).toString().getBytes();
         if(username.equals("ola") && password.equals("adeus")) {
             return 1;
         }
         else {
             return 0;
         }
+    }
+
+    public HashMap<String, String> auxLogin(String username, String password) {
+        HashMap<String, String> hmap = new HashMap<String, String>();
+        hmap.put("type", "login");
+        hmap.put("username", username);
+        hmap.put("password", password);
+        return hmap;
     }
 
     public int register(String username, String password) {
